2719e3a7beb1dd8fc051505e7cfa9dfadabff347
DrSalins <siddarthmudaliar93@gmail.com>
2022-09-21 11:14:21 +0800

    template

---
+++ apiRequest.js
@@ -0,0 +1,14 @@
+const jokeElement = document.querySelector(".jokeElement");
+const jokeBtn = document.querySelector(".jokeBtn");
+const countryElement = document.querySelector(".country");
+const populationElement = document.querySelector(".population");
+const countryBtn = document.querySelector(".countryBtn");
+const cryptoBtn = document.querySelector(".cryptoBtn");
+const coinNameSpan = document.querySelector(".coinNameSpan");
+const coinInfo = document.querySelector(".coinInfo");
+const hkdInfo = document.querySelector(".hkdInfo");
+/*Joke API*/
+
+/*RESTCountries API*/
+
+/*Coin Cap API*/
